d8b42a25fcbb2b83713279584477ad6731b3bb64
Danya Generalov <danya@danya02.ru>
2024-03-21 18:01:41 +0300

    Now JSON doesn't get buffered in memory.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use tokio::io::AsyncWriteExt;
 
 mod args;
 mod stats;
@@ -10,17 +9,15 @@ mod visitor;
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt().init();
     let args = args::Args::parse();
-    let file = tokio::fs::OpenOptions::new()
+    let file = std::fs::OpenOptions::new()
         .write(true)
         .truncate(true)
         .create(true)
         .open(args.file)
-        .await
         .unwrap();
-    let mut file = tokio::io::BufWriter::with_capacity(128 * 1024, file);
+    let file = std::io::BufWriter::with_capacity(128 * 1024, file);
     let stats = url_reader::download_url(args.url).await?;
-    let result = serde_json::to_vec(&stats)?;
-    file.write_all(&result).await?;
+    serde_json::to_writer(file, &stats)?;
 
     Ok(())
 }
